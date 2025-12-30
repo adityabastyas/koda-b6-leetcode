9a186a36825f827f2179a9d0847c2bab6b2f2af7
aditya bastyas <adityabastyasmulya@gmail.com>
2025-12-30 16:17:30 +0700

    Update task1.js

---
+++ task1.js
@@ -0,0 +1,16 @@
+// 2703. Return Length of Arguments Passed
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+  return args.length;
+};
+
+argumentsLength([5]);
+argumentsLength([{}, null, "3"]);
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
