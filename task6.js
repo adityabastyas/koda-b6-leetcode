97220c8384f849f1eaf910f108518cc88c46b8f0
aditya bastyas <adityabastyasmulya@gmail.com>
2025-12-31 10:19:42 +0700

    Add files via upload

---
+++ task6.js
@@ -0,0 +1,10 @@
+// 2634. Filter Elements from Array
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+  return arr.filter(fn);
+};
